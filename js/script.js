528e338acbbb7d1a32d970a9a131eaf6cd46d448
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-03-27 18:39:03 +0530

    uapdate

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 120) {
+// stiky  navbar
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 120) {
         $('.header').addClass('sticky');
-      } else {
+    } else {
         $('.header').removeClass('sticky');
-      }
-    });
+    }
+});
+
+// models
+function toggleModal() {
+    document.getElementById('modal').classList.toggle('hidden')
+}
