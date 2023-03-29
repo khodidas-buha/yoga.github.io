3e902fb038b5f9d1be089ba811cb53e7bd6c54e9
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-03-29 12:24:04 +0530

    update

--- js/script.js
+++ js/script.js
@@ -11,3 +11,23 @@ $(window).scroll(function () {
 function toggleModal() {
     document.getElementById('modal').classList.toggle('hidden')
 }
+
+// slider
+const config = {
+    type: 'carousel',
+    startAt: 0,
+    perView: 4,
+    gap: 32,
+    breakpoints: {
+      1280: {
+        perView: 3,
+      },
+      1024: {
+        perView: 2,
+      },
+      768: {
+        perView: 1,
+      }
+    }
+  }
+  new Glide('.glide', config).mount()
