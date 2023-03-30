fa7ae67f97b409102482b1982e79ccf188324a86
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-03-30 14:58:39 +0530

    update

--- js/script.js
+++ js/script.js
@@ -31,3 +31,23 @@ const config = {
     }
   }
   new Glide('.glide', config).mount()
+
+  // lider 2
+  const config2 = {
+    type: 'carousel',
+    startAt: 0,
+    perView: 2,
+    gap: 32,
+    breakpoints: {
+      1280: {
+        perView: 3,
+      },
+      1024: {
+        perView: 2,
+      },
+      768: {
+        perView: 1,
+      }
+    }
+  }
+  new Glide('.glide2', config2).mount()
