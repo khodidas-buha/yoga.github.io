3a8cb8bc06826b4e1d86a8e78fdbf79aad59eee2
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-04-03 16:33:24 +0530

    update

--- js/script.js
+++ js/script.js
@@ -69,3 +69,27 @@ $('.question-and-answer').click(function() {
   $(this).find(".answer").toggleClass("hidden")
   $(this).find(".question-chevron").toggleClass("hidden")
 })
+
+
+//  //Get the button
+ let mybutton = document.getElementById("btn-back-to-top");
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function () {
+   scrollFunction();
+ };
+ function scrollFunction() {
+   if (
+     document.body.scrollTop > 20 ||
+     document.documentElement.scrollTop > 20
+   ) {
+     mybutton.style.display = "block";
+   } else {
+     mybutton.style.display = "none";
+   }
+ }
+ // When the user clicks on the button, scroll to the top of the document
+ mybutton.addEventListener("click", backToTop);
+ function backToTop() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ }
